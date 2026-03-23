758edfc366d5e7851134f5dd9ecb385f63d2081e
lme79 <lme79@uclive.ac.nz>
2026-03-23 20:52:22 +1300

    US-025: add dynamic player number rendering on jersey

--- src/scripts/PlayerAgent.gd
+++ src/scripts/PlayerAgent.gd
@@ -2,9 +2,15 @@ extends CharacterBody3D
 class_name PlayerAgent
 
 @export var is_away_team: bool = false
+@export var player_number: String = "00"
+
 @onready var torso_mesh: CSGBox3D = $Armature/Torso/TorsoMesh
+@onready var number_front: Label3D = $Armature/Torso/NumberFront
+@onready var number_back: Label3D = $Armature/Torso/NumberBack
 
 func _ready() -> void:
+	number_front.text = player_number
+	number_back.text = player_number
 	var mat := StandardMaterial3D.new()
 	if is_away_team:
 		mat.albedo_color = Color(0.8, 0.2, 0.2) # Red
